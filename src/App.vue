5d0a32521d85ae7ff7971043315958dc8d2b0674
Hu Yangwei <huyangwei_vendor@DOMAIN.SENSETIME.COM>
2018-11-14 21:01:04 +0800

    树形控件雏形

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <department-tree></department-tree>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import departmentTree from '@/components/department-tree';
 export default {
-  name: 'app',
+  name: "app",
+  data() {
+    return {
+      
+    };
+  },
   components: {
-    HelloWorld
+departmentTree
+  },
+  
+
+  methods: {
+    
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less">
+  
 </style>
